6c704ae4ad14cb07a484126abd40c053e93566c9
Jeremy Mill <jeremy.mill@otis.com>
2018-07-04 20:35:29 -0400

    correctly supports ties

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # RCIR
-RCIR is a ranked choice instant runoff library written in rust. It's a work in progress
+RCIR is a ranked choice instant runoff library written in rust. It's a work in progress.
+
+In the result of a tie for last place and no winner, all the last place candidates are removed
+
+## Example
+| Voter A | Voter B | Voter C | Voter D | Voter E |
+|---------|---------|---------|---------|---------|
+| Bob     | Sue     | Bill    | Bob     | Sue     |
+| Bill    | Bob     | Sue     | Bill    | Bob     |
+| Sue     | Bill    | Bob     | Sue     | Bill    |
+
+This would result in an ElectionResult::Winner("Sue")
+
+## Tie Example
+
+| Voter A | Voter B |
+|---------|---------|
+| Bob     | Sue     |
+
+This would result in an ElectionResult::Tie(vec!["Bob", "Sue"])
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 use std::collections::VecDeque;
 use std::collections::HashMap;
 
-pub extern fn run_election<T: std::cmp::Eq + std::hash::Hash + std::clone::Clone + std::marker::Copy>(mut vote_collection: Vec<VecDeque<T>>) -> T {
-    //run passes
-    
+#[derive(Debug, PartialEq)]
+pub enum ElectionResult<T: std::cmp::Eq + std::hash::Hash + std::marker::Copy> {
+    Winner(T),
+    Tie(Vec<T>),
+}
+
+pub extern fn run_election<T: std::cmp::Eq + std::hash::Hash + std::marker::Copy>(mut vote_collection: Vec<VecDeque<T>>) -> ElectionResult<T> {
+    //run passes    
     loop {
         //vector to hold this rounds votes
         let mut roundvotes: Vec<T> = Vec::new();
@@ -22,36 +27,50 @@ pub extern fn run_election<T: std::cmp::Eq + std::hash::Hash + std::clone::Clone
         }
         //see if there's a winner
         let fifty_percent = totalvote_counter / 2;
-        let mut to_remove: Option<&T> = None;
+        //let mut to_remove: Option<&T> = None;
+        let mut to_remove_vec: Vec<&T> = Vec::new();
         let mut winner: Option<&T> = None;
         let mut to_remove_count = u64::max_value();
         for (key, votes) in &votecount {
-            if *votes >= fifty_percent {
+            if *votes > fifty_percent {
                 winner = Some(key);
                 break;
             }
+            //if this is lower than the pervious, set the low vote count
             if *votes < to_remove_count {
-                to_remove = Some(key);
                 to_remove_count = *votes;
-            } 
+                to_remove_vec.clear();
+                to_remove_vec.push(key);
+            }
+            else if *votes == to_remove_count {
+                to_remove_vec.push(key);
+            }
+
         }
         //if there's a winner, return
         if let Some(winner) = winner {
-            return *winner;
+            return ElectionResult::Winner(*winner);
         }
-        //otherwise clear the loser from the queue
-        if let Some(to_remove) = to_remove {
+        //detect if there is a tie. There is a tie if the to_remove_vec has the same
+        //length as the votecount hashmap
+        if to_remove_vec.len() == votecount.len() {
+            let mut tievec = Vec::new();
+            for tienetry in to_remove_vec {
+                tievec.push(*tienetry);
+            }
+            return ElectionResult::Tie(tievec);
+        }        
+        //otherwise clear the loser(s) from the queue
+        for removal in to_remove_vec {
             for vote in vote_collection.iter_mut() {
                 //pop the front element
                 if let Some(frontvote) = vote.pop_front() {
                     //if it's not the to_remove value, put it back
-                    if frontvote != *to_remove {
+                    if frontvote != *removal {
                         vote.push_front(frontvote);
                     }
                 }
             }
-        } else {
-            panic!("There's a tie!")
         }
     }
     
@@ -89,10 +108,30 @@ mod tests {
         voter_e.push_front("bob");
         voter_e.push_front("sue");
 
-        let mut vec = vec![voter_a, voter_b, voter_c, voter_d, voter_e];
+        let vec = vec![voter_a, voter_b, voter_c, voter_d, voter_e];
         let winner = run_election(vec);
-        println!("winner is: {}", winner);
-        assert_eq!("sue", winner);
+        println!("winner is: {:?}", winner);
+        assert_eq!(ElectionResult::Winner("sue"), winner);
 
+    }
+
+    #[test]
+    fn tie() {
+        let mut voter_a = VecDeque::new();
+        voter_a.push_front("sue");
+
+        let mut voter_b = VecDeque::new();
+        voter_b.push_front("bill");
+
+        let vec = vec![voter_a, voter_b];
+        let winner = run_election(vec);
+        let _tievec: Vec<&str> = vec!["bill", "sue"];
+        if let ElectionResult::Tie(tie_res) = winner {
+            assert!(tie_res.contains(&"bill"));
+            assert!(tie_res.contains(&"sue"));
+        }
+        else {
+            assert!(false);
+        }
     }
 }
